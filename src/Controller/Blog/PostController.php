792b9ff1c1190d6cb08a6cd28438e1ba85f010b5
Jérôme OLLIVIER <54033192+Spirecool@users.noreply.github.com>
2022-12-09 20:27:18 +0100

    Adding Search component

--- src/Controller/Blog/PostController.php
+++ src/Controller/Blog/PostController.php
@@ -3,8 +3,9 @@
 namespace App\Controller\Blog;
 
 use App\Entity\Post\Post;
+use App\Form\SearchType;
+use App\Model\SearchData;
 use App\Repository\Post\PostRepository;
-use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -15,6 +16,21 @@ class PostController extends AbstractController
     #[Route('/blog', name: 'app_post_index', methods: ['GET'])]
     public function index(PostRepository $postRepository, Request $request): Response
     {
+        // Formulaire de recherche dans le contrôleur
+        $searchData =  new SearchData();
+        $form = $this->createForm(SearchType::class, $searchData);
+
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()) {
+            $searchData->page = $request->query->getInt('page', 1);
+            $posts = $postRepository->findBySearch($searchData);
+            
+            return $this->render('pages/blog/index.html.twig', [
+                'form' => $form->createView(),
+                'posts' => $posts
+            ]);
+        }
+
         //1ère méthode
         // $posts = $postRepository->findBy(
         //     ['state' => 'STATE_PUBLISHED'],
@@ -23,8 +39,9 @@ class PostController extends AbstractController
 
         //2nde méthode
         $posts = $postRepository->findPublished($request->query->getInt('page', 1));
-                  
+
         return $this->render('pages/blog/index.html.twig', [
+            'form' => $form->createView(),
             'posts' => $posts
         ]);
     }
--- src/Repository/Post/PostRepository.php
+++ src/Repository/Post/PostRepository.php
@@ -21,13 +21,13 @@ use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
  */
 class PostRepository extends ServiceEntityRepository
 {
-    public function __construct( ManagerRegistry $registry, private PaginatorInterface $paginatorInterface) 
+    public function __construct(ManagerRegistry $registry, private PaginatorInterface $paginatorInterface)
     {
         parent::__construct($registry, Post::class);
     }
 
     // Get published posts
-    public function findPublished(int $page, ?Category $category = null, ?Tag $tag = null) : PaginationInterface
+    public function findPublished(int $page, ?Category $category = null, ?Tag $tag = null): PaginationInterface
     {
         $data = $this->createQueryBuilder('p')
             ->where('p.state LIKE :state')
@@ -35,32 +35,54 @@ class PostRepository extends ServiceEntityRepository
             ->addOrderBy('p.createdAt', 'DESC');
 
 
-            if (isset($category)) {
-                $data = $data
+        if (isset($category)) {
+            $data = $data
                 ->join('p.categories', 'c')
                 ->andWhere(':category IN (c)')
                 ->setParameter('category', $category);
-            }
+        }
 
-            if (isset($tag)) {
-                $data = $data
+        if (isset($tag)) {
+            $data = $data
                 ->join('p.tags', 't')
                 ->andWhere(':tag IN (t)')
                 ->setParameter('tag', $tag);
-            }
+        }
 
-            $data->getQuery()
-                ->getResult();
-            
-            // Pagination
-            $posts = $this->paginatorInterface->paginate(
-                $data,
-                $page,
-                9
-            );
+        $data->getQuery()
+            ->getResult();
 
-            return $posts;
+        // Pagination
+        $posts = $this->paginatorInterface->paginate(
+            $data,
+            $page,
+            9
+        );
 
+        return $posts;
+    }
+
+    public function findBySearch(SearchData $searchData): PaginationInterface
+    {
+        $data = $this->createQueryBuilder('p')
+            ->where('p.state LIKE :state')
+            ->setParameter('state', '%STATE_PUBLISHED%')
+            ->addOrderBy('p.createdAt', 'DESC');
+
+        if (!empty($searchData->q)) {
+            $data = $data
+                // ->join('p.tags', 't')
+                ->andWhere('p.title LIKE :q')
+                // ->orWhere('t.name LIKE :q')
+                ->setParameter('q', "%{$searchData->q}%");
+        }
+
+        $data = $data
+            ->getQuery()
+            ->getResult();
+
+        $posts = $this->paginatorInterface->paginate($data, $searchData->page, 9);
+
+        return $posts;
     }
 }
-
